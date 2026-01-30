9f9cc29a9ad260a5055665d9436b335d9ab6039b
sanderversteegh <sanderversteegh@users.noreply.github.com>
2026-01-30 13:34:43 +0000

    deploy: 765705d24c137b8c98455620ae0477dfddce65bb

---
+++ precache-manifest.2f54e6dc1e3a8c658a029c81e240513e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9942b6a82f7e67e9f34ef9ad4a843934",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0a2b74973ae86c97b1c",
+    "url": "./static/css/2.2678467e.chunk.css"
+  },
+  {
+    "revision": "21265efa0410513b3578",
+    "url": "./static/css/main.1d9ceb24.chunk.css"
+  },
+  {
+    "revision": "d0a2b74973ae86c97b1c",
+    "url": "./static/js/2.e462b75c.chunk.js"
+  },
+  {
+    "revision": "0ef9351e1de74923582d0ac66ffd791c",
+    "url": "./static/js/2.e462b75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21265efa0410513b3578",
+    "url": "./static/js/main.e4ea3513.chunk.js"
+  },
+  {
+    "revision": "9e8ba0d17e26a37a82b4",
+    "url": "./static/js/runtime-main.993b90c2.js"
+  }
+]);
